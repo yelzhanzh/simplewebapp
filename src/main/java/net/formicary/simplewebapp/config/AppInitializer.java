5c3ed0ee41d33c3494243942ce8550275525bfe5
yelzhanzh <yelzhan.zhangairn@formicary.net>
2015-05-27 09:58:08 +0100

    Create AppInitilizer file

---
+++ src/main/java/net/formicary/simplewebapp/config/AppInitializer.java
@@ -0,0 +1,25 @@
+package net.formicary.simplewebapp.config;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Created by Zhangariny on 26/05/2015.
+ */
+public class AppInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext container) throws ServletException {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.setServletContext(container);
+
+        ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/rest/*");
+
+    }
+
+}
