1d361bc79a2a288712e78021f1f86b0e86f2a508
yelzhanzh <yelzhan.zhangairn@formicary.net>
2015-05-27 10:27:18 +0100

    Create AppInitilizer file

--- src/main/java/net/formicary/simplewebapp/config/AppInitializer.java
+++ src/main/java/net/formicary/simplewebapp/config/AppInitializer.java
@@ -18,7 +18,7 @@ public class AppInitializer implements WebApplicationInitializer {
         ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
 
         servlet.setLoadOnStartup(1);
-        servlet.addMapping("/")
+        servlet.addMapping("/");
 
     }
 
